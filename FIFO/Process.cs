9a116a6f6fbe0ac9ae433e5b44166fd7b46a83a1
ThanhThuan2k <thuanhuynh.190800@gmail.com>
2021-04-16 09:42:05 +0700

    Add

---
+++ FIFO/Process.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+	public class Process
+	{
+		public int TimeIn { get; set; }
+		public int ProcessionTime { get; set; }
+		public string Name { get; set; }
+
+		public int RealTime { get; set; }
+		public int EndTime { get; set; }
+
+		public Process(string name, int timeIn, int process)
+		{
+			this.Name = name;
+			this.TimeIn = timeIn;
+			this.ProcessionTime = process;
+			this.RealTime = 0;
+			this.EndTime = 0;
+		}
+
+		public static void swap(List<Process> list, int i, int j)
+		{
+			Process temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+		}
+
+		public static List<Process> SortTimeIn(List<Process> list)
+		{
+			for(int i = 0; i < list.Count() - 1; i++)
+			{
+				for(int j = i + 1; j < list.Count(); j++)
+				{
+					if(list[i].TimeIn > list[j].TimeIn)
+					{
+						swap(list, i, j);
+					}
+				}
+			}
+			return list;
+		}
+
+		public static Queue<Process> ToQueue(List<Process> list)
+		{
+			Queue<Process> queue = new Queue<Process>();
+			foreach(Process item in list)
+			{
+				queue.Enqueue(item);
+			}
+			return queue;
+		}
+
+		public static int CountTime(List<Process> list)
+		{
+			int count = 0;
+			foreach(Process item in list)
+			{
+				count += item.ProcessionTime;
+			}
+			return count;
+		}
+
+		public static void setRealTime(List<Process> list)
+		{
+			// thằng đầu tiên có thời gian đợi là thời gian nó vô
+			var waitTime = list[0].TimeIn;
+			
+			for(int i = 0; i < list.Count(); i++)
+			{
+				list[i].RealTime = waitTime;
+				list[i].EndTime = waitTime + list[i].ProcessionTime;
+				waitTime += list[i].ProcessionTime;
+			}
+		}
+	}
+}
---
+++ FIFO/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.OutputEncoding = Encoding.UTF8;
+				Console.Write("Nhập số lượng tiến trình: ");
+				int numberOfProcess = Convert.ToInt32(Console.ReadLine());
+
+				List<Process> temp = new List<Process>();
+				Queue<Process> queue = new Queue<Process>();
+				for (int i = 0; i < numberOfProcess; i++)
+				{
+					Console.Write("Nhập tên tiến trình: ");
+					string text = Console.ReadLine();
+					Console.Write("Nhập thời gian vào: ");
+					int timeIn = Convert.ToInt32(Console.ReadLine());
+					Console.Write("Nhập thời gian thực thi: ");
+					int processingTime = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine();
+					Process newProcess = new Process(text, timeIn, processingTime);
+					temp.Add(newProcess);
+				}
+				// sắp xếp các tiến trình theo thứ tự tăng dần của thời gian vào
+				temp = Process.SortTimeIn(temp);
+
+				// đặt thời gian chạy thực tế cho mỗi tiến trình sau khi có đầy đủ thông tin 
+				Process.setRealTime(temp);
+
+				// bỏ vào queue theo thứ tự thời gian vào
+				queue = Process.ToQueue(temp);
+				// bắt đầu đếm
+				int totalTime = Process.CountTime(temp);
+
+				Console.WriteLine("Tổng thời gian: " + totalTime);
+				Console.WriteLine("Thứ tự các tiến trình theo thời gian: ");
+				foreach (var item in queue)
+				{
+					Console.WriteLine("Tiến trình " + item.Name);
+					Console.WriteLine("Thời gian vào thực tế: " + item.RealTime);
+					Console.WriteLine("Thời gian hoàn thành thực tế: " + item.EndTime);
+					Console.WriteLine();
+				}
+
+				Console.Read();
+			}
+			catch
+			{
+				Console.WriteLine("Vui lòng nhập số hợp lệ.");
+			}
+		}
+	}
+}
